2bc37b1fc15940d2f25e6e189009249c2337772c
borsha51 <joyahmed3623@icloud.com>
2026-09-21 00:48:17 +0600

    Delete src directory

--- src/main.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import Landing from './landing';
-import Admin from './admin';
-
-const root = createRoot(document.getElementById('root'));
-root.render(location.pathname === '/admin' ? <Admin /> : <Landing />);
